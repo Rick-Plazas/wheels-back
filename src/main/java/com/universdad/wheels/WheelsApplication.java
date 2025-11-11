f40f4cd4f628eb63152444b5686ece863af28622
lPlazas <rplazas200@hotmail.com>
2025-11-10 20:55:32 -0500

    Vuelve a modificar main para seguridad de Spring

--- src/main/java/com/universdad/wheels/WheelsApplication.java
+++ src/main/java/com/universdad/wheels/WheelsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class WheelsApplication {
 
 	public static void main(String[] args) {
